dac51cb34e28e9a97eb6d2d2404e8993b3fcdf2e
Sergio Pola <sergio.pola.contreras@gmail.com>
2017-07-07 15:53:42 -0400

    Mostrar mensaje de error  y campos requeridos

--- application/views/login/login_index.php
+++ application/views/login/login_index.php
@@ -1,11 +1,16 @@
-<form action="<?php echo site_url('login/login')?>">
+<?php if(isset($msg)):?>
+<div class="alert alert-danger" role="alert">
+    <?php echo $msg ?>
+</div>
+<?php endif ?>
+<form action="<?php echo site_url('login/validate')?>" method="post">
     <div class="form-group">
         <label for="exampleInputEmail1">Correo</label>
-        <input name="email" type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+        <input name="email" type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" required>
     </div>
     <div class="form-group">
         <label for="exampleInputPassword1">Password</label>
-        <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
     </div>
     <button type="submit" class="btn btn-default">Entrar</button>
 </form>
