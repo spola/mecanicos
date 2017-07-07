24017e4480b911ac2a7abf8f5c1d0bd749e8105a
Sergio Pola <sergio.pola.contreras@gmail.com>
2017-07-07 19:44:24 -0400

    Formato al despliegue de los datos

--- application/views/servicios/servicios_listar.php
+++ application/views/servicios/servicios_listar.php
@@ -1,3 +1,18 @@
-<pre>
-<?php var_dump($servicios) ?>
-</pre>
+<table class="table table-responsive">
+    <thead>
+        <tr>
+            <th>Nombre</th>
+            <th>Descripci&oacute;n</th>
+            <th>Precio</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($servicios as $servicio):?>
+        <tr>
+            <td><?php echo $servicio->nombre?></td>
+            <td><?php echo $servicio->descripcion?></td>
+            <td>$<?php echo $servicio->precio?></td>            
+        </tr>
+        <?php endforeach?>
+    </tbody>
+</table>
